3ff5d2d6127ce4854ce34831dbea289a4f5dfed0
すえ <souhait.0614@gmail.com>
2023-06-14 09:51:37 +0900

    react-use、eslint-plugin-react-hooks追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
-  plugins: ["@typescript-eslint", "unused-imports", "import"],
+  extends: ["next/core-web-vitals", "prettier", "plugin:react-hooks/recommended"],
+  plugins: ["@typescript-eslint", "unused-imports", "import", "react-hooks"],
   rules: {
     "eol-last": ["error", "always"],
     "@typescript-eslint/consistent-type-imports": "warn",
@@ -27,6 +27,12 @@ module.exports = {
       {
         namedComponents: "arrow-function",
         unnamedComponents: "arrow-function",
+      },
+    ],
+    "react-hooks/exhaustive-deps": [
+      "error",
+      {
+        additionalHooks: "(useAsync|useAsyncFn|useAsyncRetry)",
       },
     ],
   },
