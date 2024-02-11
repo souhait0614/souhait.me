196f05de7e5335ec6841ffa4cefa347ff5a86232
すえ <souhait.0614@gmail.com>
2024-02-11 09:41:25 +0900

    init astro

--- .prettierrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  semi: false,
-  trailingComma: "all",
-  endOfLine: "lf",
-}
--- src/app/play/souhaito-gacha/_components/GachaResultText.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css"
-
-export const emoji = style({
-  height: "2em",
-  verticalAlign: "middle",
-})
