0195acd5ec444eed6ece3e6cb00d830a4db7bc09
すえ <souhait.0614@gmail.com>
2024-02-12 13:36:25 +0900

    xoをセットアップ

---
+++ xo.config.cjs
@@ -0,0 +1,17 @@
+/** @type import("xo").Options */
+module.exports = {
+	extensions: ['astro'],
+	extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'],
+	prettier: true,
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {},
+		},
+	],
+};
