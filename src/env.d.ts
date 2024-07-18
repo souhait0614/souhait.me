6b93ef431804448c51c2dc35d0fd11859076970d
すえ <souhait.0614@gmail.com>
2024-07-19 00:53:16 +0900

    fix env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prevent-abbreviations */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="astro/client" />
