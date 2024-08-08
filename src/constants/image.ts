04a737847e19f89ba94a7afff9f66c1be670ad75
すえ <souhait.0614@gmail.com>
2024-08-08 15:37:12 +0900

    画像周りを調整

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,3 +1,5 @@
+import {getImage} from 'astro:assets';
+
 export const origin = 'https://img.souhait.me';
 
 export const icon = `${origin}/icon.png`;
@@ -12,3 +14,9 @@ export const souhaitChanOlds = [
 export const art = {
 	sakananoonigiri1: `${origin}/art/sakananoonigiri1.png`,
 } as const;
+
+export const optimizedSouhaitChan = await getImage({
+	src: souhaitChan,
+	width: 1024,
+	height: 1024,
+});
