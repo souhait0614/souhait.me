d07e7933e0929a49b0643cd4f505beed098022ba
souhait <me@souhait.me>
2025-09-28 17:15:21 +0900

    背景アニメーションの実装をJSベースに変更

--- app/features/$HyakkanoBackGround.tsx
+++ app/features/$HyakkanoBackGround.tsx
@@ -1,65 +1,75 @@
-import type { AnimationEvent } from 'hono/jsx';
-import { useEffect, useId, useMemo } from 'hono/jsx';
+import { useEffect, useId, useMemo, useRef } from 'hono/jsx';
 
 import IconHeart from '@/components/icons/IconHeart';
 import useReducedMotion from '@/hooks/useReducedMotion';
 
-const animationDuration = 20;
+const heartCount = 34;
+const baseAnimationDuration = 20;
+const skipDeltaTime = 1000;
 
-const getRandomDuration = () => {
+function getRandomDuration() {
   const randomVariation = (Math.random() * 4) - 2;
-  return `${Math.round((animationDuration + randomVariation) * 1000)}ms`;
-};
+  return Math.round((baseAnimationDuration + randomVariation) * 1000);
+}
+
+function AnimationHeart() {
+  const defaultLeft = useMemo(() => `${Math.random() * 100}%`, []);
+  const defaultTop = useMemo(() => `${Math.random() * 100}%`, []);
+  const defaultDuration = useMemo(() => getRandomDuration(), []);
+
+  const ref = useRef<SVGSVGElement>(null);
+  const durationRef = useRef(defaultDuration);
+
+  useEffect(() => {
+    const elem = ref.current;
+    if (!elem) return;
+    elem.style.left = defaultLeft;
+    elem.style.top = defaultTop;
+    elem.style.opacity = '1';
+
+    let animationFrameId = 0;
+    let previousTimestamp: number | null = null;
+
+    const animate = (timestamp: number) => {
+      const duration = durationRef.current ?? defaultDuration;
+      if (!previousTimestamp) {
+        previousTimestamp = timestamp;
+      }
+
+      let deltaTime = timestamp - previousTimestamp;
+      if (deltaTime > skipDeltaTime) deltaTime = 0;
+      previousTimestamp = timestamp;
+      const currentTop = parseFloat(elem.style.top);
+
+      const distanceToMove = (deltaTime / duration) * 100;
+      const newTop = currentTop - distanceToMove;
+      elem.style.top = `${newTop}%`;
+
+      const rect = elem.getBoundingClientRect();
+      if (rect.top + rect.height < 0) {
+        elem.style.top = '100%';
+        elem.style.left = `${Math.random() * 100}%`;
+        durationRef.current = getRandomDuration();
+      }
+
+      animationFrameId = window.requestAnimationFrame(animate);
+    };
+
+    animationFrameId = window.requestAnimationFrame(animate);
+
+    return () => window.cancelAnimationFrame(animationFrameId);
+  }, [defaultDuration, defaultLeft, defaultTop]);
+
+  return (
+    <IconHeart
+      ref={ref}
+      filled
+    />
+  );
+}
 
 export default function HyakkanoBackGround() {
   const id = useId();
-  const initialStyles = useMemo(() => Array.from({ length: 33 }, () => {
-    return {
-      left: `${Math.random() * 100}%`,
-      animationDelay: `-${animationDuration - (Math.random() * animationDuration)}s`,
-      animationDuration: getRandomDuration(),
-    };
-  }), []);
-  const handleAnimationEnd = ({ target }: AnimationEvent) => {
-    if (!(target instanceof SVGElement)) return;
-    target.style.animation = 'none';
-    target.style.visibility = 'hidden';
-    setTimeout(() => {
-      target.style.animation = '';
-      target.style.left = `${Math.random() * 100}%`;
-      target.style.animationDuration = getRandomDuration();
-      target.style.visibility = 'visible';
-    }, 1);
-  };
-  useEffect(() => {
-    const stopAnimation = () => {
-      const container = document.getElementById(id);
-      container?.childNodes.forEach((el) => {
-        if (el instanceof SVGElement) {
-          el.style.animationPlayState = 'paused';
-        }
-      });
-    };
-    const playAnimation = () => {
-      const container = document.getElementById(id);
-      container?.childNodes.forEach((el) => {
-        if (el instanceof SVGElement) {
-          el.style.animationPlayState = '';
-        }
-      });
-    };
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        stopAnimation();
-      } else {
-        playAnimation();
-      }
-    };
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, [id]);
 
   const isReducedMotion = useReducedMotion();
 
@@ -70,16 +80,11 @@ export default function HyakkanoBackGround() {
       id={id}
       class={`
         absolute top-0 left-0 h-lvh w-lvw overflow-hidden text-primary/20
-        *:absolute *:bottom-100 *:size-20 *:-translate-x-1/2
-        *:animate-hyakkano-bg-heart
+        *:absolute *:size-20 *:-translate-x-1/2 *:opacity-0
       `}
     >
-      {initialStyles.map((style) => (
-        <IconHeart
-          filled
-          onAnimationEnd={handleAnimationEnd}
-          style={style}
-        />
+      {Array.from({ length: heartCount }, (_, i) => (
+        <AnimationHeart key={i} />
       ))}
     </div>
   );
