828af54abadde0b523d0b8daa054fdf76ba10163
souhait <me@souhait.me>
2025-12-14 16:52:39 +0900

    v1を使用していたサイトへのリンクを削除

--- app/routes/play/index.tsx
+++ app/routes/play/index.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from '@/types/Metadata';
 import LinkText from '@/components/LinkText';
-import ProductCard from '@/components/ProductCard';
 
 export const metadata = {
   title: 'お遊び',
@@ -11,16 +10,20 @@ export default function Page() {
   return (
     <div class='page-container'>
       <h1 class='page-title'>{metadata.title}</h1>
-      <ProductCard TitleElement='h2' href='https://v1.souhait.me/play/souhaito-gacha' title='そうはいとガチャ'>
+      <div class='card'>
+        <h2>そうはいとガチャ (閉鎖中)</h2>
         <p>絵文字がランダムで三つ出力され、結果をMisskeyに投稿したりコピーしたりできます。</p>
         <p><s>なにこれ</s></p>
         <p><LinkText href='https://submarin.online/@f/pages/souhait' external>これ</LinkText>を勝手に移植・改造しました</p>
-      </ProductCard>
-      <ProductCard TitleElement='h2' href='https://v1.souhait.me/play/souhaito-gacha-ten' title='そうはいとガチャ 10連'>
+        <p>React Server Componentsで発覚した複数の脆弱性の影響を考慮して旧サイトを閉鎖したため、現在はアクセスできません。そのうちなんとかします</p>
+      </div>
+      <div class='card'>
+        <h2>そうはいとガチャ 10連 (閉鎖中)</h2>
         <p>絵文字がランダムで三つ×10出力され、結果をMisskeyに投稿したりコピーしたりできます。</p>
         <p><s>本当になにこれ</s></p>
         <p><LinkText href='https://submarin.online/@f/pages/souhait' external>これ</LinkText>を勝手に移植・改造しました</p>
-      </ProductCard>
+        <p>React Server Componentsで発覚した複数の脆弱性の影響を考慮して旧サイトを閉鎖したため、現在はアクセスできません。そのうちなんとかします</p>
+      </div>
     </div>
   );
 }
