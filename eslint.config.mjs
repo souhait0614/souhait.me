35b8186f17f7183a26ffbb71b9affdafbc954bcc
souhait <me@souhait.me>
2026-03-10 15:12:00 +0900

    eslint-plugin-better-tailwindcssの設定を最新化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,38 +4,11 @@ import taiymeConfig from '@taiyme/eslint-config';
 import tsEslintParser from '@typescript-eslint/parser';
 import gitignore from 'eslint-config-flat-gitignore';
 import betterTailwindcss from 'eslint-plugin-better-tailwindcss';
-import { getDefaultCallees } from 'eslint-plugin-better-tailwindcss/api/defaults';
-import { MatcherType } from 'eslint-plugin-better-tailwindcss/api/types';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 
-// TODO: 後でなんとかする
-// /** @typedef {import('eslint-plugin-better-tailwindcss/api/types').CalleeMatchers} CalleeMatchers */
-
 const files = [
   '**/*.{js,mjs,ts,tsx}',
-];
-
-// /** @type {CalleeMatchers} */
-const TV_BASE_VALUES = [
-  'tv',
-  [
-    {
-      match: MatcherType.ObjectValue,
-      pathPattern: '^base.*$',
-    },
-  ],
-];
-
-// /** @type {CalleeMatchers} */
-const TV_SLOT_VALUES = [
-  'tv',
-  [
-    {
-      match: MatcherType.ObjectValue,
-      pathPattern: '^slots.*$',
-    },
-  ],
 ];
 
 export default defineConfig(
@@ -78,22 +51,13 @@ export default defineConfig(
   },
   {
     files,
-    plugins: {
-      'better-tailwindcss': betterTailwindcss,
-    },
+    extends: [betterTailwindcss.configs.recommended],
     settings: {
       'better-tailwindcss': {
         entryPoint: './app/style.css',
-        callees: [
-          ...getDefaultCallees(),
-          TV_BASE_VALUES,
-          TV_SLOT_VALUES,
-        ],
       },
     },
     rules: {
-      ...betterTailwindcss.configs.recommended.rules,
-      'better-tailwindcss/enforce-consistent-line-wrapping': 'warn',
       'better-tailwindcss/no-unknown-classes': ['error', { detectComponentClasses: true }],
     },
   },
