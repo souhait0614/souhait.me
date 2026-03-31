13f94082ff51cb5cb13b8dcc5a4025b6ac2fcf6d
souhait <me@souhait.me>
2026-03-31 16:50:28 +0900

    100パズのイベント結果の文字列を共通化

--- app/routes/hyakkano/index.tsx
+++ app/routes/hyakkano/index.tsx
@@ -6,6 +6,10 @@ import Details from '@/components/Details';
 import LinkText from '@/components/LinkText';
 import Profile from '@/features/Profile';
 
+const puzzleEventResultContent = (eventName: string, rank: number) => {
+  return `100パズ 「${eventName}」 ${rank}位`;
+};
+
 interface EventRecord {
   date: [year: number, month: number, date: number];
   content: string;
@@ -69,7 +73,7 @@ const records: Array<EventRecord> = [
   },
   {
     date: [2025, 6, 9],
-    content: '100パズ 「きらめくジューンブライド 第1弾」 7位',
+    content: puzzleEventResultContent('きらめくジューンブライド 第1弾', 7),
     titleLink: 'https://twitter.com/souhait_100kano/status/1931922734688457018',
     labeledLinks: [
       { label: '写経', link: 'https://twitter.com/ziburikito/status/1932051196933816393' },
@@ -77,7 +81,7 @@ const records: Array<EventRecord> = [
   },
   {
     date: [2025, 8, 18],
-    content: '100パズ 「キラキラ☆マリンサマー 第2弾」 2位',
+    content: puzzleEventResultContent('キラキラ☆マリンサマー 第2弾', 2),
     titleLink: 'https://twitter.com/souhait_100kano/status/1957281687634846083',
     labeledLinks: [
       { label: '写経', link: 'https://twitter.com/ziburikito/status/1957737939443806455' },
@@ -103,7 +107,7 @@ const records: Array<EventRecord> = [
   },
   {
     date: [2025, 12, 9],
-    content: '100パズ 「恋するサンタのプレゼント大作戦♡ 第1弾」 第2位',
+    content: puzzleEventResultContent('恋するサンタのプレゼント大作戦♡ 第1弾', 2),
     titleLink: 'https://twitter.com/souhait_100kano/status/1998158810259890655',
     labeledLinks: [
       { label: '写経', link: 'https://twitter.com/ziburikito/status/1998736859690647709' },
